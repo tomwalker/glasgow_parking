4fabf319fb30c462fea55e635861c572e8eac0e7
Tom Walker <tomwalker0472@gmail.com>
2014-02-22 13:14:59 +0000

    playing around

--- controller.js
+++ controller.js
@@ -1,40 +1,31 @@
 var app = app || angular.module('parking', ['parking.services', 'ngCookies']);
 
-// app.config(['$routeProvider', '$httpProvider', 
-//             function ($routeProvider, $httpProvider) {
-//                 $httpProvider.defaults.useXDomain = true;
-//                 delete $httpProvider.defaults.headers.common['X-Requested-With'];
-//             }]);
-
-app.run(function ($http, $cookies) {
-    $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
-});
-
-app.controller('parkingController', ['$scope', '$http', 'feed',
-	function ($scope, $http, feed) {
-
-		$scope.dataRetrieve = function() {
-			feed.get({}, function (data) {
-				$scope.carfeed = angular.fromJson(data, [true]);
-				console.log('hi');
-			});
-		};
-		$scope.a = $scope.dataRetrieve();
-		$scope.cheese = 'woof';
-
-		$scope.data = "unknown";
-		$http.jsonp('http://dashboard.glasgow.gov.uk/api/live/parking.php?type=json')
-			.success(function(data){
-				$scope.data=data;
-		});
-
-		
+app.config(['$httpProvider', function($httpProvider) {
+   delete $httpProvider.defaults.headers.common["X-Requested-With"];
 }]);
 
+app.controller('parkingController', ['$scope', '$http', 'feed',
+        function ($scope, $http, feed) {
+            
+            $scope.dataRetrieve = function() {
+                feed.get({}, function (data) {
+		    console.log(data);
+                    $scope.carfeed = angular.fromJson(data, [true]);
+                    console.log('hi');
+                });
+            };
+            $scope.a = $scope.dataRetrieve();
+	    
+            $scope.cheese = 'woof';
 
+            $scope.data = "unknown";
 
+            // $http.get('http://dashboard.glasgow.gov.uk/api/live/parking.php?type=json')
+            //     .success(function(data){
+            //         $scope.data=data;
+	    // 	    console.log(data);
+            //     });
+	    
 
-
-
-
-
+        
+}]);
