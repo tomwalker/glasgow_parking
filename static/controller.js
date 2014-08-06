d7246816dc754e46772110f51ae55cde6510aed8
Tom Walker <tomwalker0472@gmail.com>
2014-08-06 23:09:13 +0100

    scrubbing away, learning more JS

--- static/controller.js
+++ static/controller.js
@@ -1,66 +1,61 @@
-'user strict';
+'use strict';
 angular
-	.module('parking', ['parking.services', 'leaflet-directive'])
+    .module('parking', ['parking.services', 'leaflet-directive'])
 
+    .controller('parkingController',
+                ['$scope', 'meters', 'glasgowcenter',
+                 function ($scope, meters, glasgowcenter) {
+                     $scope.meters = {};
+                     $scope.glasgowCenter = glasgowcenter;
 
-	.controller('parkingController',
-				['$scope', 'meters', 'MapDistance', 'glasgowcenter',
-				 function ($scope, meters, MapDistance, glasgowcenter) {
-
-					 $scope.glasgowCenter = glasgowcenter;
-
-					 $scope.meters = {};
-
-					 $scope.dataRetrieve = function(location) {
-						 console.log(meters());
-						 meters(location).success(function(markers){
-							 console.log(markers);
-						 	 $scope.meters = markers;
-						 });
-
-					 };
+                     var dataRetrieve = function(location) {
+                         meters.update(location).success(function(){
+                             $scope.meters = meters.get();
+                         });
+                     };
 
 
 
-					 if (!navigator.geolocation){
-						 $scope.current_location = false;
-						 return;
-					 }
+                     if (!navigator.geolocation){
+                         $scope.current_location = false;
+                         dataRetrieve(false);
+                     }
 
-					 function success(position) {
-						 var car_icon = {
-							 iconUrl: './car.png',
-							 iconSize:     [32, 32],
-							 iconAnchor:   [16, 16],
-							 popupAnchor:  [0, -12]
-						 };
+                     function success(position) {
+                         var car_icon = {
+                             iconUrl: './car.png',
+                             iconSize:     [32, 32],
+                             iconAnchor:   [16, 16],
+                             popupAnchor:  [0, -12]
+                         };
 
-						 var latitude  = position.coords.latitude;
-						 var longitude = position.coords.longitude;
+                         var latitude  = position.coords.latitude;
+                         var longitude = position.coords.longitude;
 
-						 $scope.meters['current'] = {
-							 lat: latitude,
-							 lng: longitude,
-							 message: 'You are here',
-							 focus: true,
-							 icon: car_icon,
-							 draggable: false
-						 };
+                         $scope.meters['current'] = {
+                             lat: latitude,
+                             lng: longitude,
+                             message: 'You are here',
+                             focus: true,
+                             icon: car_icon,
+                             draggable: false
+                         };
 
-						 $scope.current_location = {
-							 lat: latitude,
-							 lng: longitude
-						 };
-						 console.log('not error');
-						 $scope.dataRetrieve($scope.current_location);
+                         $scope.current_location = {
+                             lat: latitude,
+                             lng: longitude
+                         };
+                         console.log('not error');
+                         dataRetrieve($scope.current_location);
 
-					 }
+                     }
 
-					 function error() {
-						 console.log('error');
-						 $scope.current_location = false;
-						 $scope.dataRetrieve(false);
-					 }
-					 navigator.geolocation.getCurrentPosition(success, error);
+                     function error() {
+                         console.log('error');
+                         $scope.current_location = false;
+                         dataRetrieve(false);
+                     }
 
-				 }]);
+                     navigator.geolocation.getCurrentPosition(success, error);
+
+                 }]);
--- static/services.js
+++ static/services.js
@@ -1,119 +1,128 @@
 'use strict';
 
-function mapDistance() {
-    // taken from http://www.movable-type.co.uk/scripts/latlong.html
 
-    this.calculate = function (current, meter) {
-        Number.prototype.toRad = function() {
-            return this * Math.PI / 180;
+
+function process(){
+  return{
+    feed: function (carfeed, location) {
+
+        var mapDistance = function() {
+            // taken from http://www.movable-type.co.uk/scripts/latlong.html
+            return {
+                calculate: function (current, meter) {
+                    Number.prototype.toRad = function() {
+                        return this * Math.PI / 180;
+                    };
+                    var R = 6371;
+                    var dLat = (current.lat - meter.lat).toRad();
+                    var dLon = (current.lng - meter.lng).toRad();
+                    var lat1 = Number(meter.lat).toRad();
+                    var lat2 = Number(current.lat).toRad();
+                    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                        Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+                    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                    var d = R * c;
+                    return d.toFixed(3);
+                }
+            };
         };
 
-        var R = 6371;
-        var dLat = (current.lat - meter.lat).toRad();
-        var dLon = (current.lng - meter.lng).toRad();
-        var lat1 = Number(meter.lat).toRad();
-        var lat2 = Number(current.lat).toRad();
+        var map_markers = {};
 
-        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        var d = R * c;
-        return d.toFixed(3);
-    };
+        for (var meter in carfeed) {
+            var name = carfeed[meter].situationRecord.carParkIdentity
+                .substring(0, carfeed[meter].situationRecord.carParkIdentity.indexOf(':'))
+                .split(' ').join('_');
+
+            var latitude = carfeed[meter].situationRecord.groupOfLocations.locationContainedInGroup
+                .pointByCoordinates.pointCoordinates.latitude;
+
+            var longitude = carfeed[meter].situationRecord.groupOfLocations.locationContainedInGroup
+                .pointByCoordinates.pointCoordinates.longitude;
+
+            if (carfeed[meter].situationRecord.carParkStatus === "carParkFull"){
+                var parkingMessage = '<strong>' + name + '</strong><br>Car park full';
+
+                var full_icon = {
+                    iconUrl: './error.png',
+                    iconSize:     [32, 32],
+                    iconAnchor:   [16, 16],
+                    popupAnchor:  [0, -12]
+                };
+
+            } else {
+                var parkingMessage = '<strong>' + name + '</strong><br>Free spaces: ' +
+                    (carfeed[meter].situationRecord.totalCapacity -
+                     carfeed[meter].situationRecord.occupiedSpaces);
+
+                if (location !== false){
+                    console.log('location is not false');
+                    // var distance = mapDistance.calculate(location, {lat: latitude, lng: longitude});
+
+                    // if (distance < 1){
+                    //     parkingMessage = parkingMessage.concat('<br> Distance: ' +
+                    //                                            distance * 1000 + 'm');
+                    // } else {
+                    //     parkingMessage = parkingMessage.concat('<br> Distance: ' +
+                    //                                            distance + 'km');
+                    // }
+                }
+
+
+            }
+
+            map_markers[name] = {
+                    lat: +latitude,
+                    lng: +longitude,
+                    message: parkingMessage,
+                    focus: false,
+                    icon: full_icon || '',
+                    draggable: false
+            };
+        } // end carfeed for loop
+
+        return map_markers;
+    }
+  };
 }
 
 var glasgowCenter = {
     lat: 55.8588,
     lng: -4.2479,
     zoom: 14
-}
+};
 
 angular
-    .module('parking.services', ['ngResource'])
+  .module('parking.services', [])
+  .service('process', process)
+  .value('glasgowcenter', glasgowCenter)
 
-    .service('MapDistance', mapDistance)
+  .factory('meters', ['$http', 'process', function($http, process) {
 
-    .value('glasgowcenter', glasgowCenter)
+      var map_markers = {};
 
-    // .factory('apifeed',
-    //          ['$resource',
-    //           function($resource) {
-    //     	  return $resource('http://desolate-lowlands-9828.herokuapp.com/api',
-    //     			   null,
-    //     			   {'query':  {method:'GET', isArray:false}});
-    //           }
-    //          ])
+      var markers = function (location) {
+          location = typeof location !== 'undefined' ? location : false;
+          return $http.get('http://desolate-lowlands-9828.herokuapp.com/api')
+              .success(function (data) {
+                  var carfeed = data['payloadPublication']['situation'];
+                  var output = process.feed(carfeed, location);
+                  map_markers = output;
+              });
+      };
 
-// http://stackoverflow.com/questions/14045626/using-http-in-angular-js-factory
+      markers();
 
-    .factory('meters',
-	     function(MapDistance, $http) {
-		 return function(location) {
-		     var x = $http.get('http://desolate-lowlands-9828.herokuapp.com/api').
-			 success(function (data) {
-			     var map_markers = {};
-			     var carfeed = data['payloadPublication']['situation'];
+      var provide = function(){
+          console.log(map_markers);
+          return map_markers;
+      };
 
-			     for (var meter in carfeed) {
-				 var name = carfeed[meter].situationRecord.carParkIdentity
-				     .substring(0, carfeed[meter].situationRecord.carParkIdentity.indexOf(':'));
+      return {
 
-				 var latitude = carfeed[meter].situationRecord.groupOfLocations.locationContainedInGroup
-				     .pointByCoordinates.pointCoordinates.latitude;
+          update: markers,
+          get: provide
 
-				 var longitude = carfeed[meter].situationRecord.groupOfLocations.locationContainedInGroup
-				     .pointByCoordinates.pointCoordinates.longitude;
+      };
 
-				 if (carfeed[meter].situationRecord.carParkStatus === "carParkFull"){
-				     var parkingMessage = '<strong>' + name + '</strong><br>Car park full';
-
-				     var full_icon = {
-					 iconUrl: './error.png',
-					 iconSize:     [32, 32],
-					 iconAnchor:   [16, 16],
-					 popupAnchor:  [0, -12]
-				     };
-
-				     map_markers[name] = {
-					 lat: latitude,
-					 lng: longitude,
-					 message: parkingMessage,
-					 focus: false,
-					 icon: full_icon,
-					 draggable: false
-				     };
-
-				 } else {
-				     var parkingMessage = '<strong>' + name + '</strong><br>Free spaces: ' +
-					 (carfeed[meter].situationRecord.totalCapacity -
-					  carfeed[meter].situationRecord.occupiedSpaces);
-
-				     if (location !== false){
-					 console.log(location);
-					 var distance = MapDistance.calculate(location, {lat: latitude, lng: longitude});
-
-					 if (distance < 1){
-					     parkingMessage = parkingMessage.concat('<br> Distance: ' +
-										    distance * 1000 + 'm');
-					 } else {
-					     parkingMessage = parkingMessage.concat('<br> Distance: ' +
-										    distance + 'km');
-					 }
-				     }
-
-				     map_markers[name] = {
-					 lat: latitude,
-					 lng: longitude,
-					 message: parkingMessage,
-					 focus: false,
-					 draggable: false
-				     };
-
-				 }
-			     } // end carfeed for loop
-			     return map_markers;
-			 });
-                     console.log(x);
-                     return x.success(function(response){return response.data});
-		 };
-	     });
+  }]);
